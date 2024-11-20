31d5cf72b8049b7015362fb60b7692df5f1df475
Aryan Meher <aryanmeher5000@gmail.com>
2024-11-20 13:07:08 +0530

    Added homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import { Flex, Heading, Text } from "@radix-ui/themes";
+import { IoIosBug } from "react-icons/io";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <Flex direction="column" align="center" gap="6" p="4">
+      <IoIosBug fontSize="20vh" />
+      <Heading size="9">Issue Tracker</Heading>
+
+      <Text>
+        Our issue tracker is designed to simplify the way you manage and resolve
+        issues, whether you’re working on software development, business
+        processes, or personal projects. With an intuitive dashboard, you can
+        effortlessly track all your tasks, bugs, and issues in one central
+        place. The platform offers customizable workflows, allowing you to
+        tailor statuses and priorities to fit your specific needs. Collaboration
+        is seamless with features like task assignments, comments, and real-time
+        notifications to keep everyone on the same page. Gain valuable insights
+        into your progress through detailed analytics and reporting tools,
+        helping you make informed decisions. Built with security and reliability
+        in mind, our issue tracker scales effortlessly to accommodate teams of
+        any size. Experience a streamlined workflow and enhanced productivity
+        today!
+      </Text>
+    </Flex>
+  );
 }
