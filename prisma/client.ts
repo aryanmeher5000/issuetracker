18853d951964515a8160c8c1cb96c9b5de866911
Aryan Meher <aryanmeher5000@gmail.com>
2024-11-29 21:26:02 +0530

    correcting prisma

--- prisma/client.ts
+++ prisma/client.ts
@@ -4,12 +4,6 @@ const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = prismaClientSingleton();
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
